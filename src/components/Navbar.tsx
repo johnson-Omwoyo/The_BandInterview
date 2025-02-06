df841ad6a0b3f8fdb5f6ff7f9c208d1a846e3efe
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2025-02-06 11:06:10 +0300

    add the counter badge on the cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const navigate = useNavigate();
+
+  interface CartItem {
+    id: number;
+    quantity: number;
+  }
+  const [cartItems, setCartItems] = useState<CartItem[]>([
+    { id: 1, quantity: 2 },
+    { id: 2, quantity: 1 },
+    {
+      id: 3,
+      quantity: 3,
+    },
+  ]);
   return (
     <div className="container">
       <div className="row">
@@ -48,8 +62,17 @@ function Navbar() {
                   </li>
 
                   <li className="nav-item">
-                    <button className="btn" onClick={() => navigate("/cart")}>
+                    <button
+                      className="btn position-relative"
+                      onClick={() => navigate("/cart")}
+                    >
                       <i className="fa-solid fa-cart-shopping"></i>
+                      {cartItems.length > 0 && (
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                          {cartItems.length}
+                          <span className="visually-hidden">items in cart</span>
+                        </span>
+                      )}
                     </button>
                   </li>
                 </ul>
