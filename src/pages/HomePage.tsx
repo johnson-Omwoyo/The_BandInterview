2a8391236283e137396f65d82db46c647ea748a7
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2025-02-07 00:28:25 +0300

    Render data from a json db for offers and other products

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,70 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./HomePage.css";
-function HomePage() {
-  const carouselContainerRef = useRef(null);
+import { useNavigate } from "react-router-dom";
 
-  // Scroll to the previous card
-  const handlePrevClick = () => {
-    if (carouselContainerRef.current) {
-      const scrollAmount = carouselContainerRef.current.offsetWidth;
-      carouselContainerRef.current.scrollLeft -= scrollAmount; // Scroll left by the width of one card
-    }
+function HomePage() {
+  interface Offer {
+    id: number;
+    name: string;
+    price: number;
+    originalPrice: number;
+    discountPercentage: number;
+    description: string;
+    category: string;
+    image: string;
+    quantity: number;
+    rating: number;
+    offerEndDate: string;
+  }
+  interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    quantity: number;
+    rating: number;
+  }
+
+  const [offers, setOffers] = useState<Offer[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const filterProducts = (products: Product[], category: string) => {
+    return products.filter((product) => product.category === category);
   };
+  console.log(products);
+
+  useEffect(() => {
+    const baseUrl = "http://localhost:3000";
+    const fetchOffers = async () => {
+      try {
+        const response = await fetch(`${baseUrl}/offers`);
+        const data = await response.json();
+        setOffers(data);
+      } catch (error) {
+        console.log("Error fetching offers:", error);
+      }
+    };
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(`${baseUrl}/products`);
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log("Error fetching products:", error);
+      }
+    };
+
+    fetchOffers();
+    fetchProducts();
+  }, []);
+
+  const navigate = useNavigate();
   return (
     <div className="container-fluid my-4">
       <div className="row my-2">
-        <div className="col position-relative">
+        <div className="col-12 col-md-6 position-relative">
           <img
             src="https://themewagon.github.io/hexashop/assets/images/left-banner-image.jpg"
             alt=""
@@ -30,123 +81,56 @@ function HomePage() {
             <button className="btn">Shop Now</button>
           </div>
         </div>
-        <div className="col">
+        <div className="col-12 col-md-6">
           <div className="container-fluid ">
             <div className="row mb-4">
-              <div className="col position-relative product-card ">
-                <img
-                  src="https://themewagon.github.io/hexashop/assets/images/baner-right-image-01.jpg"
-                  alt=""
-                  className="img-fluid w-100 "
-                />
-                <div className="position-absolute top-50 w-100 justify-content-center  text-center">
-                  <span className="d-block category-name fs-2">Men</span>
-                  <span className="d-block category-description fs-5">
-                    Best Clothes For Men
-                  </span>
-                </div>
-                <div className="position-absolute d-flex div-onhover m-3 top-0 align-items-center flex-column gap-3 justify-content-center z-1">
-                  <span className="d-block category-name fs-2">Men</span>
-                  <span className="z-2 text-white category-description text-center mx-3">
-                    You wonderd how to show up to the wedding well have this on
-                  </span>
-                  <div className="gap-2 d-flex">
-                    <button className="btn btn-danger rounded-pill add-cart">
-                      <i className="fa-solid fa-cart-plus"></i>
-                    </button>
-                    <button className="btn btn-danger rounded-pill add-cart">
-                      <i className="fa-solid fa-eye"></i>
-                    </button>
+              {offers ? (
+                offers.map((offer) => (
+                  <div className="col-6 position-relative product-card my-3 ">
+                    <img
+                      src="https://themewagon.github.io/hexashop/assets/images/baner-right-image-01.jpg"
+                      alt=""
+                      className="img-fluid w-100 "
+                    />
+                    <div className="position-absolute top-50 w-100 justify-content-center  text-center">
+                      <span className="d-block category-name fs-2">
+                        {offer.name}
+                      </span>
+                      <span className="d-block category-description fs-5">
+                        {offer.description}
+                      </span>
+                    </div>
+                    <div className="position-absolute d-flex div-onhover m-3 top-0 align-items-center flex-column gap-3 justify-content-center z-1">
+                      <span className="d-block category-name fs-2">
+                        {offer.name}
+                      </span>
+                      <span className="z-2 text-white category-description text-center mx-3">
+                        {offer.description}
+                      </span>
+                      <div className="gap-2 d-flex">
+                        <button className="btn btn-danger rounded-pill add-cart">
+                          <i className="fa-solid fa-cart-plus"></i>
+                        </button>
+                        <button
+                          className="btn btn-danger rounded-pill add-cart"
+                          onClick={() =>
+                            navigate("/details", { state: { data: "data" } })
+                          }
+                        >
+                          <i className="fa-solid fa-eye"></i>
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>{" "}
-              <div className="col position-relative product-card ">
-                <img
-                  src="https://themewagon.github.io/hexashop/assets/images/baner-right-image-01.jpg"
-                  alt=""
-                  className="img-fluid w-100 "
-                />
-                <div className="position-absolute top-50 w-100 justify-content-center  text-center">
-                  <span className="d-block category-name fs-2">Men</span>
-                  <span className="d-block category-description fs-5">
-                    Best Clothes For Men
-                  </span>
-                </div>
-                <div className="position-absolute d-flex div-onhover m-3 top-0 align-items-center flex-column gap-3 justify-content-center z-1">
-                  <span className="d-block category-name fs-2">Men</span>
-                  <span className="z-2 text-white category-description text-center mx-3">
-                    You wonderd how to show up to the wedding well have this on
-                  </span>
-                  <div className="gap-2 d-flex">
-                    <button className="btn btn-danger rounded-pill add-cart">
-                      <i className="fa-solid fa-cart-plus"></i>
-                    </button>
-                    <button className="btn btn-danger rounded-pill add-cart">
-                      <i className="fa-solid fa-eye"></i>
-                    </button>
-                  </div>
-                </div>
-              </div>{" "}
-            </div>
-            <div className="row ">
-              <div className="col position-relative product-card ">
-                <img
-                  src="https://themewagon.github.io/hexashop/assets/images/baner-right-image-01.jpg"
-                  alt=""
-                  className="img-fluid w-100 "
-                />
-                <div className="position-absolute top-50 w-100 justify-content-center  text-center">
-                  <span className="d-block category-name fs-2">Men</span>
-                  <span className="d-block category-description fs-5">
-                    Best Clothes For Men
-                  </span>
-                </div>
-                <div className="position-absolute d-flex div-onhover m-3 top-0 align-items-center flex-column gap-3 justify-content-center z-1">
-                  <span className="d-block category-name fs-2">Men</span>
-                  <span className="z-2 text-white category-description text-center mx-3">
-                    You wonderd how to show up to the wedding well have this on
-                  </span>
-                  <div className="gap-2 d-flex">
-                    <button className="btn btn-danger rounded-pill add-cart">
-                      <i className="fa-solid fa-cart-plus"></i>
-                    </button>
-                    <button className="btn btn-danger rounded-pill add-cart">
-                      <i className="fa-solid fa-eye"></i>
-                    </button>
-                  </div>
-                </div>
-              </div>{" "}
-              <div className="col position-relative product-card ">
-                <img
-                  src="https://themewagon.github.io/hexashop/assets/images/baner-right-image-01.jpg"
-                  alt=""
-                  className="img-fluid w-100 "
-                />
-                <div className="position-absolute top-50 w-100 justify-content-center  text-center">
-                  <span className="d-block category-name fs-2">Men</span>
-                  <span className="d-block category-description fs-5">
-                    Best Clothes For Men
-                  </span>
-                </div>
-                <div className="position-absolute d-flex div-onhover m-3 top-0 align-items-center flex-column gap-3 justify-content-center z-1">
-                  <span className="d-block category-name fs-2">Men</span>
-                  <span className="z-2 text-white category-description text-center mx-3">
-                    You wonderd how to show up to the wedding well have this on
-                  </span>
-                  <div className="gap-2 d-flex">
-                    <button className="btn btn-danger rounded-pill add-cart">
-                      <i className="fa-solid fa-cart-plus"></i>
-                    </button>
-                    <button className="btn btn-danger rounded-pill add-cart">
-                      <i className="fa-solid fa-eye"></i>
-                    </button>
-                  </div>
-                </div>
-              </div>{" "}
+                ))
+              ) : (
+                <h1>no offers</h1>
+              )}
             </div>
           </div>
-        </div>
+        </div>{" "}
       </div>
+
       <div className="row my-5">
         <div className="col">
           <div className="container">
@@ -162,46 +146,152 @@ function HomePage() {
             </div>
             <div className="row align-items-center d-flex product-container">
               <div className="col-1">
-                <button className="btn btn-danger " onClick={handlePrevClick}>
+                <button className="btn btn-danger " onClick={() => {}}>
                   <i className="fa-solid fa-chevron-left"></i>
                 </button>
               </div>
-              <div className="col-3">
-                <img
-                  src="https://themewagon.github.io/hexashop/assets/images/men-02.jpg"
-                  alt=""
-                  className="img w-100"
-                />
-                <div className="justify-content-between d-flex">
-                  <span className="category-name text-dark">Product Name</span>
-                  <span className="category-name text-dark">*****</span>
-                </div>
-                <span className="product-header text-secondary">price</span>
-              </div>{" "}
-              <div className="col-3">
-                <img
-                  src="https://themewagon.github.io/hexashop/assets/images/men-02.jpg"
-                  alt=""
-                  className="img w-100"
-                />
-                <div className="justify-content-between d-flex">
-                  <span className="category-name text-dark">Product Name</span>
-                  <span className="category-name text-dark">*****</span>
-                </div>
-                <span className="product-header text-secondary">price</span>
-              </div>{" "}
-              <div className="col-3">
-                <img
-                  src="https://themewagon.github.io/hexashop/assets/images/men-02.jpg"
-                  alt=""
-                  className="img w-100"
-                />
-                <div className="justify-content-between d-flex">
-                  <span className="category-name text-dark">Product Name</span>
-                  <span className="category-name text-dark">*****</span>
-                </div>
-                <span className="product-header text-secondary">price</span>
+              {filterProducts(products, "Men's Clothes") ? (
+                filterProducts(products, "Men's Clothes").map((product) => (
+                  <div className="col-3">
+                    <img src={product.image} alt="" className="img w-100" />
+                    <div className="justify-content-between d-flex">
+                      <span className="category-name text-dark">
+                        {product.name}
+                      </span>
+                      <span className="category-name text-dark">*****</span>
+                    </div>
+                    <span className="product-header text-secondary">
+                      {product.price}
+                    </span>
+                  </div>
+                ))
+              ) : (
+                <h1>no products</h1>
+              )}
+
+              <div className="col-1">
+                <button className="btn btn-danger">
+                  <i className="fa-solid fa-chevron-right"></i>
+                </button>
               </div>
+            </div>{" "}
+            <div className="row my-5">
+              <div className="col">
+                <span className="category-name text-dark d-block fs-3">
+                  Women's Latest
+                </span>
+                <span className="category-description text-secondary">
+                  This will represent the description of the category
+                </span>
+              </div>
+            </div>
+            <div className="row align-items-center d-flex product-container">
+              <div className="col-1">
+                <button className="btn btn-danger " onClick={() => {}}>
+                  <i className="fa-solid fa-chevron-left"></i>
+                </button>
+              </div>
+              {filterProducts(products, "Women's Clothes") ? (
+                filterProducts(products, "Women's Clothes").map((product) => (
+                  <div className="col-3">
+                    <img src={product.image} alt="" className="img w-100" />
+                    <div className="justify-content-between d-flex">
+                      <span className="category-name text-dark">
+                        {product.name}
+                      </span>
+                      <span className="category-name text-dark">*****</span>
+                    </div>
+                    <span className="product-header text-secondary">
+                      {product.price}
+                    </span>
+                  </div>
+                ))
+              ) : (
+                <h1>no products</h1>
+              )}
+
+              <div className="col-1">
+                <button className="btn btn-danger">
+                  <i className="fa-solid fa-chevron-right"></i>
+                </button>
+              </div>
+            </div>{" "}
+            <div className="row my-5">
+              <div className="col">
+                <span className="category-name text-dark d-block fs-3">
+                  Kids Latest
+                </span>
+                <span className="category-description text-secondary">
+                  This will represent the description of the category
+                </span>
+              </div>
+            </div>
+            <div className="row align-items-center d-flex product-container">
+              <div className="col-1">
+                <button className="btn btn-danger " onClick={() => {}}>
+                  <i className="fa-solid fa-chevron-left"></i>
+                </button>
+              </div>
+              {filterProducts(products, "Kids") ? (
+                filterProducts(products, "Kids").map((product) => (
+                  <div className="col-3">
+                    <img src={product.image} alt="" className="img w-100" />
+                    <div className="justify-content-between d-flex">
+                      <span className="category-name text-dark">
+                        {product.name}
+                      </span>
+                      <span className="category-name text-dark">*****</span>
+                    </div>
+                    <span className="product-header text-secondary">
+                      {product.price}
+                    </span>
+                  </div>
+                ))
+              ) : (
+                <h1>no products</h1>
+              )}
+
+              <div className="col-1">
+                <button className="btn btn-danger">
+                  <i className="fa-solid fa-chevron-right"></i>
+                </button>
+              </div>
+            </div>{" "}
+            <div className="row my-5">
+              <div className="col">
+                <span className="category-name text-dark d-block fs-3">
+                  Jewelery{" "}
+                </span>
+                <span className="category-description text-secondary">
+                  This will represent the description of the category
+                </span>
+              </div>
+            </div>
+            <div className="row align-items-center d-flex product-container">
+              <div className="col-1">
+                <button className="btn btn-danger " onClick={() => {}}>
+                  <i className="fa-solid fa-chevron-left"></i>
+                </button>
+              </div>
+              {filterProducts(products, "Jewelry") ? (
+                filterProducts(products, "Jewelry").map((product) => (
+                  <div className="col-3">
+                    <img src={product.image} alt="" className="img w-100" />
+                    <div className="justify-content-between d-flex">
+                      <span className="category-name text-dark">
+                        {product.name}
+                      </span>
+                      <span className="category-name text-dark">*****</span>
+                    </div>
+                    <span className="product-header text-secondary">
+                      {product.price}
+                    </span>
+                  </div>
+                ))
+              ) : (
+                <h1>no products</h1>
+              )}
+
               <div className="col-1">
                 <button className="btn btn-danger">
                   <i className="fa-solid fa-chevron-right"></i>
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,4 +1,11 @@
+import { useLocation } from "react-router-dom";
+
 function ProductDetails() {
+  const location = useLocation();
+
+  const data = location.state?.data;
+  console.log(data);
+
   return <div>Product details</div>;
 }
 
